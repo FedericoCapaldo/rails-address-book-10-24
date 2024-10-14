bb181b87d6253d3e3b0d06e3ab1a7bed7c5be43d
Federico Capaldo <federico.capaldo@hotmail.com>
2024-10-14 23:31:16 +0100

    added contacts seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+Contact.destroy_all
+25.times do
+  Contact.create(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    email: Faker::Internet.email,
+    phone: Faker::PhoneNumber.phone_number,
+    birthday: Faker::Date.birthday(min_age: 18, max_age: 65)
+  )
+end
